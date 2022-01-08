3de388f98f2ca4ef2800d0f3f172838336cf1e09
Gamemap <71942164+Gamemap@users.noreply.github.com>
2022-01-08 12:42:36 +0100

    uploaded nextcloud webfolder

---
+++ nextcloud/config/config.php
@@ -0,0 +1,38 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocy72x5r79rm',
+  'passwordsalt' => 'QpehqiuAPILnQXbvBJh0xIS9E5DqI4',
+  'secret' => 'KpnUZ4jt4MuSUruuzlbHpq2bcKuaySnZMJpdCMm2wH49mwh9',
+  'trusted_domains' => 
+  array (
+    0 => '[fe80::a00:27ff:fe31:8aa3]',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '23.0.0.10',
+  'overwrite.cli.url' => 'http://nextcloud.localhost',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => '6CCSZzm[@*2K@1j9',
+  'installed' => true,
+  'default_phone_region' => 'DE',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'updater.release.channel' => 'stable',
+  'app_install_overwrite' => 
+  array (
+    0 => 'uploaddetails',
+    1 => 'caniupdate',
+    2 => 'files_3d',
+    3 => 'admin_notifications',
+    4 => 'flow_notifications',
+    5 => 'roundcube',
+    6 => 'issuetemplate',
+  ),
+  'default_language' => 'de',
+  'maintenance' => false,
+  'updater.secret' => '$2y$10$0wO5PmNVeEgYgC6f9pnLI.ThEDQG11qqN69vY3N0hAGCfl.8vSzi6',
+);
